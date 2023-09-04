682db8337f0ddac4967679610711cca4218996f6
John <134107082+jksnook@users.noreply.github.com>
2023-09-04 12:38:55 -0700

    add subtract, multiply, and divide functions to script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// adds two numbers together
 function add(a, b) {
   return a + b;
+}
+
+// subtracts one number from another
+function subtract(a, b) {
+  return a - b;
+}
+
+// multiplies two numbers together
+function multiply(a, b) {
+  return a * b;
+}
+
+// divides number a by number b
+function divide(a, b) {
+  return a / b;
 }
